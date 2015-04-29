6611709f6568ab9b775d473cc993c15e25dcf791
nheath99 <njh86@outlook.com>
2015-04-29 12:48:37 +0100

    Initial

---
+++ EquipmentManager/Controllers/EquipmentController.cs
@@ -0,0 +1,35 @@
+﻿using EquipmentManager.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EquipmentManager.Controllers
+{
+    public class EquipmentController : Controller
+    {
+        EquipmentManagerEntities db = new EquipmentManagerEntities();
+
+        public ActionResult Index()
+        {
+            var equipment = db.Equipments.ToList();
+            return View(equipment);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            var equipment = db.Equipments.Find(id.Value);
+            if (equipment == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            return View(equipment);
+        }
+    }
+}
